2c418be7d6216361fe6c47bf47b6b57189e4281c
Tunde Ojebisi <realtundeonline@gmail.com>
2022-02-12 15:17:34 +0000

    Initial commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How many countries are in Africa?",
+    answer: "54",
+    options: [
+      "53",
+      "54",
+      "55",
+      "56"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Austria is located at which continent?",
+    answer: "Europe",
+    options: [
+      "Africa",
+      "Australia/Oceania",
+      "Europe",
+      "Asis"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The capital of Brasil is?",
+    answer: "Brasilla",
+    options: [
+      "Brasilla",
+      "Bahia",
+      "Sao Paulo",
+      "Rio Grande do Norte"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of this is found in an egg?",
+    answer: "Yoke",
+    options: [
+      "Yoke",
+      "Bin",
+      "Slate",
+      "Mayonnese"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who is the current president of Russia?",
+    answer: "Vladimir Putin",
+    options: [
+      "Oleg Ashavin",
+      "Vladimir Putin",
+      "Boris Johnson",
+      "Angela Markel"
+    ]
+  }
+]
